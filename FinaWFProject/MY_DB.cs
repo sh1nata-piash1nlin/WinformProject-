dfcb1dd073d4b38f0a05fdcebf37a8c436efeb82
sh1nata <tri14102004@gmail.com>
2025-04-01 11:04:24 +0700

    add MY_DB.cs

---
+++ FinaWFProject/MY_DB.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinaWFProject
+{
+    internal class MY_DB
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=QLSVDB;Integrated Security=True;Connect Timeout=30;");
+        // get the connection
+        public SqlConnection getConnection
+        {
+            get { return con; }
+
+        }
+        //open the connection 
+        public void openConnection()
+        {
+            if ((con.State == ConnectionState.Closed))
+            {
+                con.Open();
+            }
+        }
+        //close the connection 
+        public void closeConnection()
+        {
+            if ((con.State == ConnectionState.Open))
+            {
+                con.Close();
+            }
+        }
+    }
+}
